88acba0773215493e100524d52547ce0bf252444
b b <bboutette@gmail.com>
2017-04-24 12:37:22 -0500

    homework complete

--- src/App.js
+++ src/App.js
@@ -1,55 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Keys from './components/Keys.js';
+import Display from './components/Display.js'
+//make separate componenets, keys, display, then import them in the parent component
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>React Calculator!</h1>
-        <div className="box">
-          <div className="title">
-            <p>GA Instruments TI-WDI</p>
-          </div>
-          <div className="display">
-            <input type="text" readOnly size="18" id="d"/>
-          </div>
-          <div className="keys">
-            <p>
-              <input type="button" className="button gray" value="mrc" />
-              <input type="button" className="button gray" value="m-" />
-              <input type="button" className="button gray" value="m+" />
-              <input type="button" className="button pink" value="/" />
-            </p>
-            <p>
-            <input type="button" className="button black" value="7" />
-              <input type="button" className="button black" value="8" />
-                <input type="button" className="button black" value="9" />
-                <input type="button" className="button pink" value="*" />
-              </p>
-              <p>
-                <input type="button" className="button black" value="4" />
-                <input type="button" className="button black" value="5" />
-                  <input type="button" className="button black" value="6" />
-                  <input type="button" className="button pink" value="-" />
-                  </p>
-                <p>
-                  <input type="button" className="button black" value="1" />
-                <input type="button" className="button black" value="2" />
-                  <input type="button" className="button black" value="3" />
-                  <input type="button" className="button pink" value="+" />
-
-                  </p>
-                <p>
-                  <input type="button" className="button black" value="0" />
-                <input type="button" className="button black" value="." />
-                  <input type="button" className="button black" value="C" />
-                  <input type="button" className="button orange" value="=" />
-
-                  </p>
-              </div>
-            </div>
-          </div>
-    );
+  render(){
+    return(
+      <div className="container">
+        <p className="title"> GA Instruments TI-WDI </p>
+        <Display />
+        <Keys />
+      </div>
+    )
   }
 }
 
---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../index.css';
+
+
+class Display extends Component {
+  render(){
+    return(
+      <input type="text" readOnly size="18" id="d"/>
+    )
+  }
+}
+
+
+export default Display;
